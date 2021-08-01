de1be040d96b6fc31cbaa2f2c92e518b90634527
longle2812 <lehailong2812@gmail.com>
2021-08-01 15:36:29 +0700

    add firebase module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { LoaderStartComponent } from './shared/loader-start/loader-start.component';
 import { SlideMenuComponent } from './shared/slide-menu/slide-menu.component';
 import {HeaderComponent} from './shared/header/header.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireStorageModule} from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import {HeaderComponent} from './shared/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   exports: [
