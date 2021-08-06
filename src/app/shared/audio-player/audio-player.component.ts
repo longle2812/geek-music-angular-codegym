b3639851be920f03ae313d51921d99fb7b39efa6
longle2812 <lehailong2812@gmail.com>
2021-08-06 19:06:36 +0700

    fix delete current playing song

--- src/app/shared/audio-player/audio-player.component.ts
+++ src/app/shared/audio-player/audio-player.component.ts
@@ -40,103 +40,107 @@ export class AudioPlayerComponent implements OnInit {
             shuffleOnLoop: true,
           }
         });
-        $('#jquery_jplayer_1').on($.jPlayer.event.ready + ' ' + $.jPlayer.event.play, (event) => {
-          var current = this.myPlaylist.current;
-          var playlist = this.myPlaylist.playlist;
-          $.each(playlist, function(index, obj) {
-            if (index == current) {
-              $('.jp-now-playing').html('<div class=\'jp-track-name\'><span class=\'que_img\'><img style=\'width: 50px; height: 50px\' id=\'jp_playing_img\' src=\'' + obj.image + '\'></span><div class=\'que_data\'><span id=\'jp_playing_title\'>' + obj.title + '</span> <div class=\'jp-artist-name\'> <span id=\'jp_playing_artist\'>' + obj.artist + '</span></div></div></div>');
-            }
-          });
-          $('.que_close').click(() => {
-            console.log('1');
-            // myPlaylist.remove(0);
-          });
-          $('.knob-wrapper').mousedown(() => {
-            $(window).mousemove((e) => {
-              var angle1 = getRotationDegrees($('.knob')),
-                volume = angle1 / 270;
-
-              if (volume > 1) {
-                $('#jquery_jplayer_1').jPlayer('volume', 1);
-              } else if (volume <= 0) {
-                $('#jquery_jplayer_1').jPlayer('mute');
-              } else {
-                $('#jquery_jplayer_1').jPlayer('volume', volume);
-                $('#jquery_jplayer_1').jPlayer('unmute');
-              }
-            });
-            return false;
-          }).mouseup(() => {
-            $(window).unbind('mousemove');
-          });
-
-
-          function getRotationDegrees(obj) {
-            var matrix = obj.css('-webkit-transform') ||
-              obj.css('-moz-transform') ||
-              obj.css('-ms-transform') ||
-              obj.css('-o-transform') ||
-              obj.css('transform');
-            if (matrix !== 'none') {
-              var values = matrix.split('(')[1].split(')')[0].split(',');
-              var a = values[0];
-              var b = values[1];
-              var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
-            } else {
-              var angle = 0;
-            }
-            return (angle < 0) ? angle + 360 : angle;
-          }
-
-          var timeDrag = false;
-          $('.jp-play-bar').mousedown((e) => {
-            timeDrag = true;
-            updatebar(e.pageX);
-
-          });
-          $(document).mouseup((e) => {
-            if (timeDrag) {
-              timeDrag = false;
-              updatebar(e.pageX);
-            }
-          });
-          $(document).mousemove((e) => {
-            if (timeDrag) {
-              updatebar(e.pageX);
-            }
-          });
-          var updatebar = (x) => {
-            var progress = $('.jp-progress');
-            var position = x - progress.offset().left;
-            var percentage = 100 * position / progress.width();
-            if (percentage > 100) {
-              percentage = 100;
-            }
-            if (percentage < 0) {
-              percentage = 0;
-            }
-            $('#jquery_jplayer_1').jPlayer('playHead', percentage);
-            $('.jp-play-bar').css('width', percentage + '%');
-          };
-          $('#playlist-toggle, #playlist-text, #playlist-wrap li a').unbind().on('click', () => {
-            $('#playlist-wrap').fadeToggle();
-            $('#playlist-toggle, #playlist-text').toggleClass('playlist-is-visible');
-          });
-          $('.hide_player').unbind().on('click', () => {
-            $('.audio-player').toggleClass('is_hidden');
-            $(this).html($(this).html() == '<i class="fa fa-angle-down"></i> HIDE' ? '<i class="fa fa-angle-up"></i> SHOW PLAYER' : '<i class="fa fa-angle-down"></i> HIDE');
-          });
-          $('body').unbind().on('click', '.audio-play-btn', function() {
-            $('.audio-play-btn').removeClass('is_playing');
-            $(this).addClass('is_playing');
-            var playlistId = $(this).data('playlist-id');
-            this.myPlaylist.play(playlistId);
-          });
-        });
+        this.render();
       }
     });
     ;
+  }
+
+  private render() {
+    $('#jquery_jplayer_1').on($.jPlayer.event.ready + ' ' + $.jPlayer.event.play, (event) => {
+      var current = this.myPlaylist.current;
+      var playlist = this.myPlaylist.playlist;
+      $.each(playlist, function(index, obj) {
+        if (index == current) {
+          $('.jp-now-playing').html('<div class=\'jp-track-name\'><span class=\'que_img\'><img style=\'width: 50px; height: 50px\' id=\'jp_playing_img\' src=\'' + obj.image + '\'></span><div class=\'que_data\'><span id=\'jp_playing_title\'>' + obj.title + '</span> <div class=\'jp-artist-name\'> <span id=\'jp_playing_artist\'>' + obj.artist + '</span></div></div></div>');
+        }
+      });
+      $('.que_close').click(() => {
+        console.log('1');
+        // myPlaylist.remove(0);
+      });
+      $('.knob-wrapper').mousedown(() => {
+        $(window).mousemove((e) => {
+          var angle1 = getRotationDegrees($('.knob')),
+            volume = angle1 / 270;
+
+          if (volume > 1) {
+            $('#jquery_jplayer_1').jPlayer('volume', 1);
+          } else if (volume <= 0) {
+            $('#jquery_jplayer_1').jPlayer('mute');
+          } else {
+            $('#jquery_jplayer_1').jPlayer('volume', volume);
+            $('#jquery_jplayer_1').jPlayer('unmute');
+          }
+        });
+        return false;
+      }).mouseup(() => {
+        $(window).unbind('mousemove');
+      });
+
+
+      function getRotationDegrees(obj) {
+        var matrix = obj.css('-webkit-transform') ||
+          obj.css('-moz-transform') ||
+          obj.css('-ms-transform') ||
+          obj.css('-o-transform') ||
+          obj.css('transform');
+        if (matrix !== 'none') {
+          var values = matrix.split('(')[1].split(')')[0].split(',');
+          var a = values[0];
+          var b = values[1];
+          var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+        } else {
+          var angle = 0;
+        }
+        return (angle < 0) ? angle + 360 : angle;
+      }
+
+      var timeDrag = false;
+      $('.jp-play-bar').mousedown((e) => {
+        timeDrag = true;
+        updatebar(e.pageX);
+
+      });
+      $(document).mouseup((e) => {
+        if (timeDrag) {
+          timeDrag = false;
+          updatebar(e.pageX);
+        }
+      });
+      $(document).mousemove((e) => {
+        if (timeDrag) {
+          updatebar(e.pageX);
+        }
+      });
+      var updatebar = (x) => {
+        var progress = $('.jp-progress');
+        var position = x - progress.offset().left;
+        var percentage = 100 * position / progress.width();
+        if (percentage > 100) {
+          percentage = 100;
+        }
+        if (percentage < 0) {
+          percentage = 0;
+        }
+        $('#jquery_jplayer_1').jPlayer('playHead', percentage);
+        $('.jp-play-bar').css('width', percentage + '%');
+      };
+      $('#playlist-toggle, #playlist-text, #playlist-wrap li a').unbind().on('click', () => {
+        $('#playlist-wrap').fadeToggle();
+        $('#playlist-toggle, #playlist-text').toggleClass('playlist-is-visible');
+      });
+      $('.hide_player').unbind().on('click', () => {
+        $('.audio-player').toggleClass('is_hidden');
+        $(this).html($(this).html() == '<i class="fa fa-angle-down"></i> HIDE' ? '<i class="fa fa-angle-up"></i> SHOW PLAYER' : '<i class="fa fa-angle-down"></i> HIDE');
+      });
+      $('body').unbind().on('click', '.audio-play-btn', function() {
+        $('.audio-play-btn').removeClass('is_playing');
+        $(this).addClass('is_playing');
+        var playlistId = $(this).data('playlist-id');
+        this.myPlaylist.play(playlistId);
+      });
+    });
   }
 
   ngOnInit() {
@@ -147,9 +151,11 @@ export class AudioPlayerComponent implements OnInit {
         }
         else if (queue.title === 'delete'){
           let i = 0;
+          let index = -1;
           while ( i < this.queue.length){
             if (this.queue[i].id == queue.songId){
               this.queue.splice(i, 1);
+              index = i;
               break;
             }
             i++;
@@ -171,6 +177,7 @@ export class AudioPlayerComponent implements OnInit {
           $('#jp_playing_img').attr('src','assets/images/album/album.jpg');
           $('#jp_playing_title').text('');
           this.myPlaylist.play(0);
+           setTimeout( () => this.myPlaylist.play(0),1000)
         }
         else if (queue.song !== undefined) {
           this.songService.getSongById(queue.songId).subscribe(
@@ -187,7 +194,7 @@ export class AudioPlayerComponent implements OnInit {
                 if (queue.title === 'add' && queue.songId !== -1){
                   this.queue.push(song);
                   this.myPlaylist.add(queue.song);
-                  console.log(this.queue);
+                  console.log(this.myPlaylist.current);
                 }
                 if (queue.title === 'play' && queue.songId !== -1){
                   this.myPlaylist.add(queue.song,[true]);
