6599f5137d7e0d4539278dd995b7ed0b5b17d383
Just625 <bloodseekerofdota@gmail.com>
2021-08-12 00:15:32 +0700

    bat user nhap lai mat khau cu truoc khi doi mat khau

--- src/app/content/change-password/change-password.component.ts
+++ src/app/content/change-password/change-password.component.ts
@@ -4,7 +4,9 @@ import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/form
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {User} from '../../model/user';
 import {NotificationService} from '../../service/notification/notification.service';
+
 declare var $: any;
+
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
@@ -12,6 +14,7 @@ declare var $: any;
 })
 export class ChangePasswordComponent implements OnInit {
   id: number;
+  emptyError = '';
   pwGroup: FormGroup = new FormGroup({
     password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]),
     confirmPassword: new FormControl('')
@@ -36,20 +39,29 @@ export class ChangePasswordComponent implements OnInit {
   }
 
   changePass() {
-    if (this.pwGroup.valid) {
-      // tslint:disable-next-line:prefer-const
-      let user: User = {password: ''};
-      user.password = this.pwGroup.value.password;
-      this.userService.changePassword(this.id, user).subscribe(() => {
-        this.notificationService.showSuccessMessage('Success');
-        this.pwGroup.reset();
-      }, (e) => {
-        if (e.error === 'duplicate password') {
-          this.notificationService.showErrorMessage('New password can not be your old password');
-        } else {
-          this.notificationService.showErrorMessage('Can not change password');
-        }
-      });
+    const oldPass = $('#old_pass').val();
+    this.pwGroup.markAllAsTouched();
+    if (oldPass.length === 0) {
+      this.emptyError = 'This is required';
+    } else {
+      this.emptyError = '';
+      if (this.pwGroup.valid && oldPass.length > 0) {
+        // tslint:disable-next-line:prefer-const
+        let user: User = {password: ''};
+        user.password = this.pwGroup.value.password;
+        this.userService.changePassword(this.id, user, oldPass).subscribe(() => {
+          this.notificationService.showSuccessMessage('Success');
+          this.pwGroup.reset();
+        }, (e) => {
+          if (e.error === 'duplicate password') {
+            this.notificationService.showErrorMessage('New password can not be your old password');
+          } else if (e.error === 'old pass not match') {
+            this.notificationService.showErrorMessage('Your old password is not correct!');
+          } else {
+            this.notificationService.showErrorMessage('Can not change password');
+          }
+        });
+      }
     }
   }
 
@@ -61,12 +73,15 @@ export class ChangePasswordComponent implements OnInit {
   togglePassword() {
     const passInput = $('#password_input')[0];
     const confirmPassInput = $('#confirm_pass_input')[0];
+    const oldPassInput = $('#old_pass')[0];
     if (passInput.type === 'password') {
       passInput.type = 'text';
       confirmPassInput.type = 'text';
+      oldPassInput.type = 'text';
     } else {
       passInput.type = 'password';
       confirmPassInput.type = 'password';
+      oldPassInput.type = 'password';
     }
   }
 }
