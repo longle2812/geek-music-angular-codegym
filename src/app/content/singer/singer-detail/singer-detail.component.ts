db6601ad5980acfa52e3565b7366376d0163a954
Just625 <bloodseekerofdota@gmail.com>
2021-08-11 11:00:04 +0700

    Comment trong singer detail

--- src/app/content/singer/singer-detail/singer-detail.component.ts
+++ src/app/content/singer/singer-detail/singer-detail.component.ts
@@ -1,13 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {Playlist} from '../../../model/playlist';
 import {UserToken} from '../../../model/user-token';
 import {ActivatedRoute, Router} from '@angular/router';
-import {PlaylistService} from '../../../service/playlist/playlist.service';
 import {AuthenticationService} from '../../../service/authentication/authentication.service';
 import {Singer} from '../../../model/singer';
 import {SingerService} from '../../../service/singer/singer.service';
 import {Song} from '../../../model/song';
 import {SongService} from '../../../service/song/song.service';
+import {SingerInteraction} from '../../../model/singer-interaction';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SongInteraction} from '../../../model/song-interaction';
+import {NotificationService} from '../../../service/notification/notification.service';
+import {SocketService} from '../../../service/socket/socket.service';
+import {UserService} from '../../../service/user/user.service';
+import {User} from '../../../model/user';
+
+declare var $: any;
 
 @Component({
   selector: 'app-singer-detail',
@@ -15,20 +22,31 @@ import {SongService} from '../../../service/song/song.service';
   styleUrls: ['./singer-detail.component.css']
 })
 export class SingerDetailComponent implements OnInit {
+  URL = '/songs/detail/';
   singer: Singer = {};
   userToken: UserToken = {};
   songs: Song[] = [];
+  commentForm: FormGroup = new FormGroup({
+    comment: new FormControl('', [Validators.required])
+  });
+  comments: SongInteraction[];
+  page = 0;
+  size = 5;
+  scrollPercent = 0.796;
 
   constructor(private activatedRouter: ActivatedRoute,
               private singerService: SingerService,
               private router: Router,
               private authenticationService: AuthenticationService,
-              private songService: SongService
-  ) {
+              private songService: SongService,
+              private notificationService: NotificationService,
+              private socketService: SocketService,
+              private userService: UserService) {
     this.activatedRouter.paramMap.subscribe(paramMap => {
-      const id = paramMap.get('id');
+      const id = +paramMap.get('id');
       this.getSinger(id);
       this.getSongs(id);
+      this.getSingerComment(id);
     });
     this.authenticationService.currentUserSubject.subscribe(user => {
       this.userToken = user;
@@ -48,12 +66,16 @@ export class SingerDetailComponent implements OnInit {
   private getSongs(id) {
     this.songService.getSongBySingerId(id).subscribe(songs => {
       this.songs = songs;
-      console.log('songs ', songs.length);
+      // console.log('songs ', songs.length);
     });
   }
 
   ngOnInit() {
+    this.loadScript('/assets/js/menu-slider.js');
     this.loadScript('/assets/js/more-option.js');
+    this.size = 5;
+    this.checkScrollDownBottom();
+    this.scrollPercent = 0.796;
   }
 
   public loadScript(url: string) {
@@ -66,5 +88,62 @@ export class SingerDetailComponent implements OnInit {
     body.appendChild(script);
   }
 
+  addComment() {
+    this.commentForm.markAllAsTouched();
+    if (this.userToken === null) {
+      this.notificationService.showErrorMessage('Need to login first before comment');
+    }
+    if (this.commentForm.valid && this.userToken !== null) {
+      this.singerService.addSingerComment(this.userToken.id, this.singer.id,
+        this.commentForm.value.comment).subscribe(songInteraction => {
+        if (this.userToken.id !== this.singer.user.id) {
+          const notification = {
+            sender: {
+              id: this.userToken.id
+            },
+            recieverId: this.singer.user.id,
+            link: this.URL + this.singer.id,
+            content: this.commentForm.value.comment,
+            action: 'commented on your song: ' + this.singer.name
+          };
+          this.socketService.createNotificationUsingSocket(notification);
+        }
 
+        this.commentForm.reset();
+
+        this.getSingerComment(this.singer.id);
+      }, e => {
+        console.log(e);
+      });
+    }
+  }
+
+  getSingerComment(id: number) {
+    this.singerService.getSingerComment(id, this.page, this.size).subscribe((comments: SingerInteraction[]) => {
+      this.comments = comments;
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.comments.length; i++) {
+        const currentComment = this.comments[i];
+        const a = new Date(currentComment.createdAt);
+        a.setMonth(a.getMonth() + 1);
+        currentComment.createdAt = a.getDate() + '/' + a.getMonth() + '/' + a.getFullYear() + ' ' + a.getHours() + ':' + a.getMinutes();
+        this.userService.findById(currentComment.senderId).subscribe((sender: User) => {
+          currentComment.sender = sender;
+        });
+      }
+    }, e => {
+      console.log(e);
+    });
+  }
+
+  checkScrollDownBottom() {
+    $(window).scroll(() => {
+      if ($(window).scrollTop() + $(window).height() >= ($(document).height() * this.scrollPercent)) {
+        $(window).scrollTop($(window).scrollTop() - 20);
+        this.size += 5;
+        this.getSingerComment(this.singer.id);
+        this.scrollPercent += 0.017;
+      }
+    });
+  }
 }
--- src/app/model/singer-interaction.ts
+++ src/app/model/singer-interaction.ts
@@ -1,2 +1,14 @@
+import {User} from './user';
+
 export interface SingerInteraction {
+  id?: number;
+  senderId?: number;
+  receiverId?: number;
+  playlistId?: number;
+  comment?: string;
+  createdAt?: string;
+  link?: string;
+  likes?: boolean;
+  isRead?: boolean;
+  sender?: User;
 }
